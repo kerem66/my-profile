621ea9635f0a54f7ee5471d7aaaae174ba237099
Salih Kerem Harman <keremharman66@gmail.com>
2020-02-18 21:39:22 +0300

    Updates

---
+++ precache-manifest.5aed528d551402da16365bca00ebcad7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e402f331dab1772639e3",
+    "url": "/my-profile/static/js/runtime~main.e402f331.js"
+  },
+  {
+    "revision": "50092a7a143eec948aae",
+    "url": "/my-profile/static/js/main.50092a7a.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/my-profile/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "50092a7a143eec948aae",
+    "url": "/my-profile/static/css/main.53c62b55.chunk.css"
+  },
+  {
+    "revision": "07a226df0f9dde99dca9aae45624657a",
+    "url": "/my-profile/index.html"
+  }
+];
